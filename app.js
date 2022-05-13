9afbfa78e8f20c26fb3933bf465bb69479aa3c3e
Mitch Driesenga <mitchdriesenga@gmail.com>
2022-05-13 08:56:14 -0400

    first develop push

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ const printProfileData = profileDataArr => {
 
   console.log('==========');
 
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
+  profileDataArr.forEach(profileItem => console.log(profileItem));
 };
 
 printProfileData(profileDataArgs);
