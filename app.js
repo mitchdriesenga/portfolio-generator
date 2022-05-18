f243732c9d328da4965c304b86e0c18924af77c5
Mitch Driesenga <mitchdriesenga@gmail.com>
2022-05-17 20:10:44 -0400

    added inquire package

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'boobies',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-const [name, github] = profileDataArgs;
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
 
+// const pageHTML = genereatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-});
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+
+// });
